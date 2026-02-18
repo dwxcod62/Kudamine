2a87c77affcee562947f13c9c658518b0b077467
Nguyễn Anh Đức <116811415+dwxcod62@users.noreply.github.com>
2026-02-17 19:19:05 -0700

    update

--- backend/prisma/migrations/20250925190344_add_pricealert_compound_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,securityId]` on the table `PriceAlert` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "PriceAlert_userId_securityId_key" ON "public"."PriceAlert"("userId", "securityId");
